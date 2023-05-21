2671f0cd4582c4da788d103369db22d670e3ddff
KMK_HM <kyun9151@gmail.com>
2023-05-21 23:27:31 +0900

    feat: 회원가입 기능 구현

--- src/main/java/jscode/board/controller/MemberController.java
+++ src/main/java/jscode/board/controller/MemberController.java
@@ -24,7 +24,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @ApiOperation(value = "회원가입 기능", notes = "이메일과 비밀번호를 입력받아 게시글 생성")
-    @PostMapping("/signup")
+    @PostMapping("/sign-up")
     public ResponseEntity<MemberResponseDto> signup(@Valid @RequestBody MemberRequestDto req) {
         MemberResponseDto member = memberService.signup(req);
         return new ResponseEntity(member, HttpStatus.CREATED);
